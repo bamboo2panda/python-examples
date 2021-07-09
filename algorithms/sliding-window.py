fa25e15cf7dd8f2b1fb869cf1b6ca713f083273c
Pavel <bamboo2panda@gmail.com>
2021-07-10 00:04:28 +0300

    Добавлен пример Найти симую длинную последовательность из n букв

--- algorithms/sliding-window.py
+++ algorithms/sliding-window.py
@@ -30,3 +30,26 @@ def find_min_subarray_length(arr: list, target_sum: int) -> int:
     return min_length
 
 print(find_min_subarray_length([4, 2, 1, 7, 8, 1, 0], 8))
+
+
+# Найти симую длинную последовательность из n букв
+
+def find_longest_sequence(arr: list, length: int) -> int:
+    longest_subsiquence = 0
+    window_start_index = 0
+    char_frequancy_map = {}
+    for window_end_index, window_end_value in enumerate(arr):
+        char_frequancy_map.update({window_end_value: char_frequancy_map.get(window_end_value, 0) + 1})
+        
+        while(len(char_frequancy_map) > length):
+            print(char_frequancy_map)
+            left_char = arr[window_start_index]
+            char_frequancy_map.update({left_char: char_frequancy_map.get(left_char) - 1})
+            if char_frequancy_map.get(left_char) == 0:
+                char_frequancy_map.pop(left_char)
+            window_start_index += 1
+        longest_subsiquence = max(longest_subsiquence, window_end_index - window_start_index + 1)
+    return longest_subsiquence
+
+
+print (find_longest_sequence(['a', 'a', 'a', 'h', 'h', 'i', 'b', 'c'], 2))
